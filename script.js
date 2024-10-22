974ef6b664cd34db976739e4bcaa95a2e8b07672
Kinji <linhqwertyuiop2gmail.com>
2024-10-22 14:43:37 +0700

    first update

---
+++ script.js
@@ -0,0 +1,19 @@
+document
+  .getElementById("speakButton")
+  .addEventListener("click", async function () {
+    const text = document.getElementById("text").value;
+
+    try {
+      const res = await axios.get(
+        `https://api.voicerss.org/?key=dc34e6febedf4aab9d6e7c194c272302&hl=vi-vn&src=${text}`
+      );
+
+      const urlAudio = res.config.url;
+
+      const audioPlayer = document.getElementById("audioPlayer");
+      audioPlayer.src = urlAudio;
+      audioPlayer.play();
+    } catch (error) {
+      console.log(error);
+    }
+  });
