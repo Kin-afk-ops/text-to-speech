0dd3197a04ecf507ca989bd991dbcd6fc2f489a8
Kinji <linhqwertyuiop2gmail.com>
2024-10-28 22:31:53 +0700

    update 28_10_2024

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
+function updateRateLabel(value) {
+  document.getElementById("rateValue").textContent = value;
+}
+
 document
   .getElementById("speakButton")
   .addEventListener("click", async function () {
     const text = document.getElementById("text").value;
 
-    try {
-      const res = await axios.get(
-        `https://api.voicerss.org/?key=dc34e6febedf4aab9d6e7c194c272302&hl=vi-vn&src=${text}`
-      );
+    const audioContainer = document.getElementById("audioContainer");
 
-      const urlAudio = res.config.url;
+    audioContainer.classList.toggle("active");
 
-      const audioPlayer = document.getElementById("audioPlayer");
-      audioPlayer.src = urlAudio;
-      audioPlayer.play();
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   const res = await axios.get(
+    //     `https://api.voicerss.org/?key=dc34e6febedf4aab9d6e7c194c272302&hl=vi-vn&src=${text}`
+    //   );
+
+    //   const urlAudio = res.config.url;
+
+    //   const audioPlayer = document.getElementById("audioPlayer");
+    //   audioPlayer.src = urlAudio;
+    //   audioPlayer.play();
+    // } catch (error) {
+    //   console.log(error);
+    // }
   });
