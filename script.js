c09df80d1ce2c3c1378342a3c70dd6f554c6ea55
Kinji <linhqwertyuiop2gmail.com>
2024-10-29 14:56:48 +0700

    complete

--- script.js
+++ script.js
@@ -2,26 +2,36 @@ function updateRateLabel(value) {
   document.getElementById("rateValue").textContent = value;
 }
 
+let urlAudio = "";
+
 document
   .getElementById("speakButton")
   .addEventListener("click", async function () {
+    const rateValue = document.getElementById("rateValue");
     const text = document.getElementById("text").value;
+    const selectElement = document.getElementById("country-select");
 
     const audioContainer = document.getElementById("audioContainer");
 
-    audioContainer.classList.toggle("active");
+    try {
+      const res = await axios.get(
+        `https://api.voicerss.org/?key=dc34e6febedf4aab9d6e7c194c272302&hl=${selectElement.value}&src=${text}&r=${rateValue.value}`
+      );
 
-    // try {
-    //   const res = await axios.get(
-    //     `https://api.voicerss.org/?key=dc34e6febedf4aab9d6e7c194c272302&hl=vi-vn&src=${text}`
-    //   );
+      urlAudio = res.config.url;
 
-    //   const urlAudio = res.config.url;
+      const audioPlayer = document.getElementById("audioPlayer");
+      audioPlayer.src = urlAudio;
+      audioPlayer.play();
+      audioContainer.classList.toggle("active");
+    } catch (error) {
+      alert("lỗi rồi");
+      console.log(error);
+    }
+  });
 
-    //   const audioPlayer = document.getElementById("audioPlayer");
-    //   audioPlayer.src = urlAudio;
-    //   audioPlayer.play();
-    // } catch (error) {
-    //   console.log(error);
-    // }
+document
+  .getElementById("download-button")
+  .addEventListener("click", async function () {
+    window.open(urlAudio);
   });
